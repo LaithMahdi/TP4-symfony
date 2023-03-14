7e402bfe8ef777d27790eba4b96b2d68e5680cc3
Laith Mahdi <109853134+LaithMahdi@users.noreply.github.com>
2023-03-14 22:40:46 +0100

    problem with getDoctrine

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,7 +1,8 @@
 <?php
 namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 Use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
@@ -13,12 +14,23 @@ class IndexController extends AbstractController
     //    return $this->render("index.html.twig",['name'=>$name]);
     // }
     #[Route('/', name: 'home')]
-
+        
     public function home()
     {
         $articles = ['Artcile1', 'Article 2','Article 3'];
         return $this->render('articles/index.html.twig',['articles' => $articles]);
     }
+    #[Route('/article/save', name: 'save')]
+    public function save() {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = new Article();
+        $article->setNom('Article 1');
+        $article->setPrix(1000);
+        $entityManager->persist($article);
+        $entityManager->flush();
+        return new Response('Article enregisté avec id '.$article->getId());
+    }
+    
 }
 
 ?>
