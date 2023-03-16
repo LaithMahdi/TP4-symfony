b125d8be719c62a5b81282cc36f154fa2b48db17
Laith Mahdi <109853134+LaithMahdi@users.noreply.github.com>
2023-03-16 09:44:27 +0100

    finish

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -15,12 +15,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class IndexController extends AbstractController
 {
     private $entityManager;
-    private $formFactory;
+    
 
 
-    public function __construct(EntityManagerInterface $entityManager,FormFactoryInterface $formFactory) {
+    public function __construct(EntityManagerInterface $entityManager) {
         $this->entityManager = $entityManager;
-        $this->formFactory = $formFactory;
+        
     }
 
         
@@ -33,16 +33,16 @@ class IndexController extends AbstractController
         return $this->render('articles/index.html.twig',['articles'=> $articles]);
     }
 
-    #[Route('/articles/create', name: 'new_article',methods: ["GET"])]
-    public function new(Request $request)
+    #[Route('/articles/create', name: 'new_article',methods: ["POST","GET"])]
+    public function new(Request $request,FormFactoryInterface $formFactory)
     {
     $article = new Article();
-    $form =  $this->formFactory->createFormBuilder($article)->add('nom', TextType::class)->add('prix', TextType::class)->add('save', SubmitType::class, array('label' => 'Créer'))->getForm();
+    $form =  $this->createFormBuilder($article)->add('nom', TextType::class)->add('prix', TextType::class)->add('save', SubmitType::class, array('label' => 'Créer'))->getForm();
    
     $form->handleRequest($request);
     if($form->isSubmitted() && $form->isValid()) {
         $article = $form->getData();
-        $this->entityManager->getDoctrine()->getManager();
+        //$this->entityManager->getDoctrine()->getManager();
         $this->entityManager->persist($article);
         $this->entityManager->flush();
         return $this->redirectToRoute('article_list');
@@ -51,15 +51,58 @@ class IndexController extends AbstractController
     }
 
 
-    #[Route('/article/save', name: 'save')]
-    public function save() {
+    #[Route('/article/edit/{id}', name: 'edit_article',methods: ["POST","GET"])]
+    public function edit(Request $request, $id,FormFactoryInterface $formFactory) {
         $article = new Article();
-        $article->setNom('Article 3');
-        $article->setPrix(00);
-        $this->entityManager->persist($article);
+        $article = $this->entityManager->getRepository(Article::class)->find($id);
+    
+        $form = $this->createFormBuilder($article)
+        ->add('nom', TextType::class)
+        ->add('prix', TextType::class)
+        ->add('save', SubmitType::class, array('label' => 'Modifier'))->getForm();
+    
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
         $this->entityManager->flush();
-        return new Response('Article enregisté avec id '.$article->getId());
+    
+        return $this->redirectToRoute('article_list');
+        }  
+        return $this->render('articles/edit.html.twig', ['form' => $form->createView()]);
     }
+        
+
+  
+    #[Route('/article/delete/{id}', name: 'delete_article')]
+    public function delete(Request $request, $id) {
+        $article = $this->entityManager->getRepository(Article::class)->find($id);
+    
+        //$entityManager = $this->getDoctrine()->getManager();
+        $this->entityManager->remove($article);
+        $this->entityManager->flush();
+    
+        $response = new Response();
+        $response->send();
+        return $this->redirectToRoute('article_list');
+    }
+   
+    
+    #[Route('/article/{id}', name: 'article_show')]
+    public function show($id) {
+        $article = $this->entityManager->getRepository(Article::class)->find($id);
+        return $this->render('articles/show.html.twig',array('article' => $article));
+    }
+    
+
+
+    // #[Route('/article/save', name: 'save')]
+    // public function save() {
+    //     $article = new Article();
+    //     $article->setNom('Article 3');
+    //     $article->setPrix(00);
+    //     $this->entityManager->persist($article);
+    //     $this->entityManager->flush();
+    //     return new Response('Article enregisté avec id '.$article->getId());
+    // }
     
 
 
